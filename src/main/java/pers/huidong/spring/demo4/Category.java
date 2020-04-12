46958aa96914b432c1f2bcc54b68bfa9104c3c7c
Test <2804234243@qq.com>
2020-04-12 23:06:10 +0800

    SpringIoc学习的第三天，第二次提交：bean的属性注入的方式

---
+++ src/main/java/pers/huidong/spring/demo4/Category.java
@@ -0,0 +1,26 @@
+package pers.huidong.spring.demo4;
+
+/**
+ * @program: spring_ioc
+ * @auther: HuiDong
+ * @date: 2020/4/12 22:22
+ * @description: Spel的属性注入
+ */
+public class Category {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/pers/huidong/spring/demo4/SpringDemo4.java
@@ -0,0 +1,54 @@
+package pers.huidong.spring.demo4;
+
+import com.sun.javaws.IconUtil;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * @program: spring_ioc
+ * @auther: HuiDong
+ * @date: 2020/4/12 19:01
+ * @description: Spring属性注入测试
+ */
+public class SpringDemo4 {
+
+    /**
+    * @Description: Bean的构造方法的属性注入
+    */
+    @Test
+    public void demo1() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        User user = (User) applicationContext.getBean("user");
+        System.out.println(user);
+    }
+    /**
+    * @Description: set方法的属性注入
+    */
+    @Test
+    public void demo2() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = (Person) applicationContext.getBean("person");
+        System.out.println(person);
+    }
+    /**
+     * @Description: p名称空间的属性注入
+     */
+    @Test
+    public void demo3() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = (Person) applicationContext.getBean("person");
+        System.out.println(person);
+    }
+    /**
+     * @Description: SpEL的属性注入
+     */
+    @Test
+    public void demo4() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Product product = (Product) applicationContext.getBean("product");
+        System.out.println(product);
+    }
+}
